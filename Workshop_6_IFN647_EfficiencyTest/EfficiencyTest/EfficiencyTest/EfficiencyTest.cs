a15ef4c7c78ed4dd7eee8c6d74a67ba7cff7a578
yuxin <zyx954@gmail.com>
2016-09-07 20:19:38 +1000

    week7

---
+++ Workshop_6_IFN647_EfficiencyTest/EfficiencyTest/EfficiencyTest/EfficiencyTest.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EfficiencyTest
+{
+    class EfficiencyTest
+    {
+        Dictionary<string, bool> stopwordList = new Dictionary<string, bool>();
+        PorterStemmerAlgorithm.PorterStemmer myStemmer = new PorterStemmerAlgorithm.PorterStemmer();
+        private void initializeStopWords()
+        {
+            stopwordList.Add("a", true);
+            stopwordList.Add("an", true);
+            stopwordList.Add("and", true);
+            stopwordList.Add("are", true);
+            stopwordList.Add("as", true);
+            stopwordList.Add("at", true);
+            stopwordList.Add("be", true);
+            stopwordList.Add("but", true);
+            stopwordList.Add("by", true);
+            stopwordList.Add("for", true);
+            stopwordList.Add("if", true);
+            stopwordList.Add("in", true);
+            stopwordList.Add("into", true);
+            stopwordList.Add("is", true);
+            stopwordList.Add("it", true);
+            stopwordList.Add("no", true);
+            stopwordList.Add("not", true);
+            stopwordList.Add("of", true);
+            stopwordList.Add("on", true);
+            stopwordList.Add("or", true);
+            stopwordList.Add("such", true);
+            stopwordList.Add("that", true);
+            stopwordList.Add("the", true);
+            stopwordList.Add("their", true);
+            stopwordList.Add("then", true);
+
+            stopwordList.Add("there", true);
+            stopwordList.Add("these", true);
+            stopwordList.Add("they", true);
+            stopwordList.Add("this", true);
+            stopwordList.Add("to", true);
+            stopwordList.Add("was", true);
+            stopwordList.Add("will", true);
+            stopwordList.Add("with", true);
+        }
+
+        EfficiencyTest()
+        {
+        
+        }
+
+        /// <summary>
+        /// Sends the thread to sleep and outputs the sleep time
+        /// </summary>
+        public void testSleepEfficiency(string text){
+            //Console.WriteLine("Sending system to sleep...zzzz");
+            DateTime start = System.DateTime.Now;
+            //System.Threading.Thread.Sleep(millisleep);
+            string[] strs = TestLinguisticProcessEfficiency(text);
+            DateTime end = System.DateTime.Now;
+            Console.WriteLine("Time to tokenlise" + (end - start));
+
+            DateTime start2 = System.DateTime.Now;
+            //System.Threading.Thread.Sleep(millisleep);
+          string[] strsNonStopWords = RemoveStopWords(strs);
+            DateTime end2 = System.DateTime.Now;
+            Console.WriteLine("Time to RemoveStopWords" + (end2 - start2));
+
+            DateTime start3 = System.DateTime.Now;
+            //System.Threading.Thread.Sleep(millisleep);
+            StemTokens(strsNonStopWords);
+            DateTime end3 = System.DateTime.Now;
+            Console.WriteLine("Time to RemoveStopWords" + (end3 - start3));
+
+        }
+
+        public string[] TestLinguisticProcessEfficiency(string s)
+        {
+            string[] strs = TokeniseString(s);
+            return strs;
+
+            
+        }
+
+        public string[] TokeniseString(string text)
+        {
+            // stub
+            text = text.ToLower();
+            return text.Split(new char[] { ' '}, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+
+        public string[] RemoveStopWords(string[] tokens)
+        {
+            List<string> filteredList = new List<string>();
+
+            foreach (string token in tokens)
+            {
+                if (token.Length > 2)
+                {
+                    if (!stopwordList.ContainsKey(token))
+                    {
+                        filteredList.Add(token);
+                    }
+                }
+            }
+
+            return filteredList.ToArray();
+        }
+
+
+        public string[] StemTokens(string[] tokens)
+        {
+            List<string> array = new List<string>();
+            foreach (string token in tokens)
+            {
+               // Console.WriteLine("Orginal: " + token);
+                string temp = myStemmer.stemTerm(token);
+                array.Add(temp);
+               // Console.WriteLine("Stems: " + temp);
+            }
+            return array.ToArray();
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            EfficiencyTest test = new EfficiencyTest();
+            test.testSleepEfficiency(AliceText.Text);
+            
+            Console.ReadLine();
+        }
+
+
+
+    }
+}
---
+++ Workshop_6_IFN647_TestLuceneEfficiency/TestLuceneEfficiency/TestLuceneEfficiency/TestLuceneEfficiency.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Lucene.Net.Analysis; // for Analyser
+using Lucene.Net.Documents; // for Document and Field
+using Lucene.Net.Index; //for Index Writer
+using Lucene.Net.Store; //for Directory
+
+namespace EfficiencyTest
+{
+    class TestLuceneEfficiency
+    {
+
+        Lucene.Net.Store.Directory luceneIndexDirectory;
+        Lucene.Net.Analysis.Analyzer analyzer;
+        Lucene.Net.Index.IndexWriter writer;
+
+
+        const Lucene.Net.Util.Version VERSION = Lucene.Net.Util.Version.LUCENE_30;
+        const string TEXT_FN = "Text";
+
+        public TestLuceneEfficiency()
+        {
+            luceneIndexDirectory = null;
+            analyzer = new Lucene.Net.Analysis.Standard.StandardAnalyzer(VERSION);
+            writer = null;
+
+        }
+
+        /// <summary>
+        /// Creates the index at indexPath
+        /// </summary>
+        /// <param name="indexPath">Directory path to create the index</param>
+        public void CreateIndex(string indexPath)
+        {
+            luceneIndexDirectory = Lucene.Net.Store.FSDirectory.Open(indexPath);
+            IndexWriter.MaxFieldLength mfl = new IndexWriter.MaxFieldLength(IndexWriter.DEFAULT_MAX_FIELD_LENGTH);
+            writer = new Lucene.Net.Index.IndexWriter(luceneIndexDirectory, analyzer,true, mfl);
+
+        }
+
+
+        /// <summary>
+        /// Indexes the given text
+        /// </summary>
+        /// <param name="text">Text to index</param>
+        public void IndexText(string text)
+        {
+            //Field field = new Field("Text", text, Field.Store.NO, Field.Index.ANALYZED_NO_NORMS, Field.TermVector.NO);
+            Field field = new Field("Text", text, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.YES);
+            Document doc = new Document();
+            doc.Add(field);
+            writer.AddDocument(doc);
+     
+        }
+
+
+        /// <summary>
+        /// Flushes buffer and closes the index
+        /// </summary>
+        public void CleanUpIndexer()
+        {
+            writer.Optimize();
+            writer.Flush(true, true, true);
+            writer.Dispose();
+        }
+
+        static void Main(string[] args)
+        {
+
+            TestLuceneEfficiency myLuceneApp = new TestLuceneEfficiency();
+
+            // TODO: ADD PATHNAME
+            string indexPath = @"./Outcome";
+
+            myLuceneApp.CreateIndex(indexPath);
+
+            System.Console.WriteLine("Adding Documents to Index");
+
+            int numDocs = 1000;
+            string text = "A"; 
+
+            for (int i = 0; i < numDocs; i++ )
+            {
+                myLuceneApp.IndexText(text);
+            }
+
+            System.Console.WriteLine("All documents added.");
+
+            // clean up
+            myLuceneApp.CleanUpIndexer();
+
+
+            System.Console.ReadLine();
+        }
+    }
+}
